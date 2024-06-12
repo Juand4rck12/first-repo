ae2276c0a079836c0cb205ac2b0a3c51a16df60b
Juan_Diego <juan3312orrego@gmail.com>
2024-06-12 12:03:37 -0500

    Cambios practica

--- tarea_4/main.js
+++ tarea_4/main.js
@@ -1,8 +1,11 @@
 // Números del 1 al 2000 que empiezan por 2:
-let num = 0;
-for(count = 1; count <= 2000; count++) {
-    num++;
-    if(num.toString().startsWith("2")){
+let num = 2000;
+for(let count = 1; count <= num; count++) {
+    let numToString = count.toString();
+
+    if(numToString.startsWith(2)){
         console.log(num);
     }
 }
+
+console.log(typeof numToString)
