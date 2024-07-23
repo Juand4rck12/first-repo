66b57c5d453fa9f76dab7ef76f64d301342ee5a9
Juan_Diego <juan3312orrego@gmail.com>
2024-07-22 20:46:44 -0500

    cambios actualizados.

--- practicaDOM/app.js
+++ practicaDOM/app.js
@@ -1,1 +1,23 @@
-let div = document.querySelector("div")
+let div = document.querySelector('div');
+let boton = document.getElementById('boton');
+
+boton.addEventListener('click',saludar);
+boton.addEventListener('click',adios);
+
+// boton.onclick = saludar;
+// boton.onclick = adios;
+
+function colores () {
+    console.log('entro -->');
+    div.innerHTML = 'Esto lo trae el JavaScript';
+    div.style.marginTop = '50px';
+    div.style.background = 'aliceblue';
+}
+
+function saludar () {
+    alert('Hola estoy aqui');
+}
+
+function adios () {
+    alert('adios');
+}
