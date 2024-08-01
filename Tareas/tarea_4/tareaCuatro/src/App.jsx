31ec6fd87ee46c15e4c3541e2f527084c176183d
Juan_Diego <juan3312orrego@gmail.com>
2024-08-01 12:10:02 -0500

    tarea 4 finalizada.

--- Tareas/tarea_4/tareaCuatro/src/App.jsx
+++ Tareas/tarea_4/tareaCuatro/src/App.jsx
@@ -3,15 +3,19 @@ import './App.css'
 import RickAndMortyCard from "./components/card/rickAndMortyCard"
 
 function App() {
-  
+
   return (
-    <div className="papa">
-      <div className="tituloPagina">
+    <div>
       <h1>PERSONAJES DE RICK AND MORTY</h1>
-      <RickAndMortyCard id={1}/>
-      <RickAndMortyCard id={2}/>
-      <RickAndMortyCard id={3}/>
-      <RickAndMortyCard id={4}/>
+      <div className="cards-page">
+        <RickAndMortyCard id={1} />
+        <RickAndMortyCard id={2} />
+        <RickAndMortyCard id={3} />
+        <RickAndMortyCard id={4} />
+        <RickAndMortyCard id={5} />
+        <RickAndMortyCard id={7} />
+        <RickAndMortyCard id={8} />
+        <RickAndMortyCard id={9} />
       </div>
     </div>
   )
--- Tareas/tarea_4/tareaCuatro/src/components/showCard/characterCard.jsx
+++ Tareas/tarea_4/tareaCuatro/src/components/showCard/characterCard.jsx
@@ -3,14 +3,10 @@ import React from 'react'
 const CharacterCard = (props) => {
   return (
     <div className='card'>
-        <h1>{props.name}</h1>
-        <img src={props.image} alt="image" />
-        <p>
-            <b>Gender: {props.gender}</b>
-        </p>
-        <p>
-            <b>Status: {props.status}</b>
-        </p>
+      <h1>{props.name}</h1>
+      <img src={props.image} alt="image" />
+      <p><b>Gender: </b>{props.gender}</p>
+      <p><b>Status: </b>{props.status}</p>
     </div>
   )
 }
