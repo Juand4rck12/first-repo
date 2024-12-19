3c8b26f6cd17d3b360475e41d02b6f786cecf5af
Juan_Diego <juan3312orrego@gmail.com>
2024-12-19 17:53:28 -0500

    practica curso analisis de datos con python

---
+++ Practicas/practicas_python/numeros.py
@@ -0,0 +1,50 @@
+import numpy as np, pandas as pd
+
+data = [50,50,47,97,49,3,53,42,26,74,82,62,37,15,70,27,36,35,48,52,63,64]
+print(data)
+
+grades = np.array(data)
+print(grades)
+
+print('\n---\n')
+
+print (type(data), 'x 2:', data * 2)
+print('---')
+print(type(data), 'x 2:', data * 2)
+
+grades.shape
+grades[0]
+grades.mean
+
+# Define an array of study hours
+study_hours = [10.0,11.5,9.0,16.0,9.25,1.0,11.5,9.0,8.5,14.5,15.5,
+               13.75,9.0,8.0,15.5,8.0,9.0,6.0,10.0,12.0,12.5,12.0]
+
+# Create a 2D array (an array of arrays)
+student_data = np.array([study_hours, grades])
+
+# display the array
+student_data
+
+# Show shape of 2D array
+student_data.shape
+
+# Show the first element of the first element
+student_data[0][0]
+
+# Get the mean value of each sub-array
+avg_study = student_data[0].mean()
+avg_grade = student_data[1].mean()
+
+print('Average study hours: {:.2f}\nAverage grade: {:.2f}'.format(avg_study, avg_grade))
+
+df_students = pd.DataFrame({'Name': ['Dan', 'Joann', 'Pedro', 'Rosie', 'Ethan', 'Vicky', 'Frederic', 'Jimmie', 
+                                     'Rhonda', 'Giovanni', 'Francesca', 'Rajab', 'Naiyana', 'Kian', 'Jenny',
+                                     'Jakeem','Helena','Ismat','Anila','Skye','Daniel','Aisha'],
+                            'StudyHours':student_data[0],
+                            'Grade':student_data[1]})
+
+df_students 
+
+# Get the data for index value 5
+df_students.loc[5]
