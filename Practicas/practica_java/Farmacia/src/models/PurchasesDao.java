cef0b65bacf93449b81fd73609958b50301e96ab
Juan_Diego <juan3312orrego@gmail.com>
2025-01-08 18:28:42 -0500

    metodos agregados en purchasesDao

--- Practicas/practica_java/Farmacia/src/models/PurchasesDao.java
+++ Practicas/practica_java/Farmacia/src/models/PurchasesDao.java
@@ -10,6 +10,8 @@ import java.sql.ResultSet;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 
@@ -88,5 +90,64 @@ public class PurchasesDao {
             System.err.println(e.getMessage());
         }
         return id;
+    }
+    
+    // Listar todas las compras realizadas
+    public List listAllPurchasesQuery() {
+        List<Purchases> list_purchase = new ArrayList();
+        String query = "SELECT purc.*, supp.name AS supplier_name FROM purchases purc, suppliers supp "
+                + "WHERE purc.supplier_id = supp.id ORDER BY purc.id ASC";
+        
+        try {
+            conn = cn.getConnection();
+            pst = conn.prepareStatement(query);
+            rs = pst.executeQuery();
+            
+            while (rs.next()) {
+                Purchases purchase = new Purchases();
+                purchase.setId(rs.getInt("id"));
+                purchase.setSupplier_name_product(rs.getString("supplier_name"));
+                purchase.setTotal(rs.getDouble("total"));
+                purchase.setCreated(rs.getString("created"));
+                list_purchase.add(purchase);
+            }
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        return list_purchase;
+    }
+    
+    // Listar compras para imprimir factura
+    public List listPurchaseDetailQuery(int id) {
+        List<Purchases> list_purchases = new ArrayList();
+        String query = "select pu.created, pude.purchase_price, pude.purchase_amount, pude.purchase_subtotal, su.name AS supplier_name,\n" +
+                        "pro.name AS product_name, em.full_name FROM purchases pu INNER JOIN purchase_details pude ON pu.id = pude.purchase_id\n" +
+                        "INNER JOIN products pro ON pude.product_id = pro.id INNER JOIN suppliers su ON supplier_id = su.id\n" +
+                        "INNER JOIN employess em ON pu.employe_id = em.id WHERE pu.id = ?";
+        
+        try {
+            conn = cn.getConnection();
+            pst = conn.prepareStatement(query);
+            pst.setInt(1, id);
+            rs = pst.executeQuery();
+            
+            while (rs.next()) {
+                Purchases purchase = new Purchases();
+                purchase.setProduct_name(rs.getString("product_name"));
+                purchase.setPurchase_amount(rs.getInt("purchase_amount"));
+                purchase.setPurchase_price(rs.getDouble("purchase_price"));
+                purchase.setPurchase_subtotal(rs.getDouble("purchase_subtototal"));
+                purchase.setSupplier_name_product(rs.getString("supplier_name"));
+                purchase.setCreated(rs.getString("created"));
+                purchase.setPurchaser(rs.getString("full_name"));
+                list_purchases.add(purchase);
+                
+            }
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        return list_purchases;
     }
 }
