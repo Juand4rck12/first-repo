b8c07d05698125479f2c34b70c4831a50f300498
Juan_Diego <juan3312orrego@gmail.com>
2025-02-25 19:36:55 -0500

    solo inserto el archivo de practicas

---
+++ Practicas/practica_java/AlgorithmPractices/src/main/java/co/edu/sena/algorithmpractices/AlgorithmPractices.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.sena.algorithmpractices;
+
+/**
+ * Fecha: Inicie el 25/02/2025
+ * @author Juan Diego Orrego Vargas
+ * Objetivo: Practicar algoritmos de programación, usando Java
+ */
+
+public class AlgorithmPractices {
+    public static void main(String[] args) {
+        System.out.println("ay no");
+    }
+}
