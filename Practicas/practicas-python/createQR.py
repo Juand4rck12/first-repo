badd1d41ad976c6956abd27e85776c4f61145f6b
Juan_Diego <juan3312orrego@gmail.com>
2025-12-17 15:26:57 -0500

    QR actualizado

--- Practicas/practicas-python/createQR.py
+++ Practicas/practicas-python/createQR.py
@@ -1,7 +1,7 @@
 import qrcode
 
 # URL a la que quieres que apunte el QR
-url = "https://wsp.registraduria.gov.co/estado_docs/documento/consultar/"
+url = "https://www.google.com/maps/place/La+Hueca+Manabita/@40.7511067,-73.8745812,18z/data=!4m6!3m5!1s0x89c25f817d731d6b:0x9018f539ad4e44ac!8m2!3d40.7510694!4d-73.8733313!16s%2Fg%2F11gxtywhc7?entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoASAFQAw%3D%3D"
 
 # Crear el objeto QR
 qr = qrcode.QRCode(
